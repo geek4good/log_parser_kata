8cbac5bc97aa8d25c2314787aef2fa1bd70fbefd
Lucas Mbiwe <lucas@geek4good.com>
2019-01-24 22:03:34 +0000

    Get rid of redundant example spec

--- spec/log_parser_kata_spec.rb
+++ spec/log_parser_kata_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe LogParserKata do
   it "has a version number" do
     expect(LogParserKata::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
